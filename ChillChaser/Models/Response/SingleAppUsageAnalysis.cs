75eac47a16ea00664cc327d819c9ee2cf109f1e4
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-05-17 12:16:41 +0200

    Made referential stress nullable and made averagestress mandatory

--- ChillChaser/Models/Response/SingleAppUsageAnalysis.cs
+++ ChillChaser/Models/Response/SingleAppUsageAnalysis.cs
@@ -1,7 +1,7 @@
 ﻿namespace ChillChaser.Models.Response {
 	public class SingleAppUsageAnalysis {
 		public required DateTime AppUsageStart { get; set; }
-		public required double AverageStress { get; set; }
+		public required double? AverageStress { get; set; }
 		public required double ReferenceStress { get; set; }
 		public required TimeSpan Usage { get; set; }
 	}
