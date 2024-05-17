f815eae5e0f140ad173fa62043a0241bbee8a7f7
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-05-17 12:18:16 +0200

    Made the fix do what i want

--- ChillChaser/Models/Response/SingleAppUsageAnalysis.cs
+++ ChillChaser/Models/Response/SingleAppUsageAnalysis.cs
@@ -1,8 +1,8 @@
 ﻿namespace ChillChaser.Models.Response {
 	public class SingleAppUsageAnalysis {
 		public required DateTime AppUsageStart { get; set; }
-		public required double? AverageStress { get; set; }
-		public required double ReferenceStress { get; set; }
+		public required double AverageStress { get; set; }
+		public required double? ReferenceStress { get; set; }
 		public required TimeSpan Usage { get; set; }
 	}
 }
