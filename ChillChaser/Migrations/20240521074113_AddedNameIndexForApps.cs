2f5b1d3899819b06d3e2d3f628e003ab936c1bd2
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-05-21 09:46:36 +0200

    added index (#44)

---
+++ ChillChaser/Migrations/20240521074113_AddedNameIndexForApps.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChillChaser.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNameIndexForApps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "name_unique_idx",
+                table: "Apps",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "name_unique_idx",
+                table: "Apps");
+        }
+    }
+}
--- ChillChaser/Models/DB/App.cs
+++ ChillChaser/Models/DB/App.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace ChillChaser.Models.DB
 {
-    public class App
-    {
+	[Index(nameof(Name), IsUnique = true, Name = "name_unique_idx")]
+    public class App {
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Notification> Notifications { get; set; } = [];
