9668b13a6e3d25ea5dc6e7065ee4f633eae84964
JE-FH <5774453+JE-FH@users.noreply.github.com>
2024-05-22 18:33:05 +0200

    Added index to views and sped up query big time

---
+++ ChillChaser/Migrations/20240522154904_AddViewIndices.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChillChaser.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddViewIndices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            CREATE UNIQUE INDEX "ReferencialStress_id_ui"
+                ON "ReferencialStress" ("AppUsageId");
+            """);
+
+            migrationBuilder.Sql("""
+            CREATE UNIQUE INDEX "AppUsageStress_id_ui"
+                ON "AppUsageStress" ("AppUsageId");
+            """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+            DROP INDEX "ReferencialStress_id_ui"
+            """);
+
+            migrationBuilder.Sql("""
+            DROP INDEX "AppUsageStress_id_ui"
+            """);
+        }
+    }
+}
