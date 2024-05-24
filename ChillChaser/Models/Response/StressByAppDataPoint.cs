21f4780860a132a8fa05e7c941c4ee74f1de9b11
Jens Emil Fink Højriis <5774453+JE-FH@users.noreply.github.com>
2024-05-24 08:19:30 +0200

    Added endpoint to reset test users (#48)

--- ChillChaser/Models/Response/StressByAppDataPoint.cs
+++ ChillChaser/Models/Response/StressByAppDataPoint.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChillChaser.Models.Response {
 	public class StressByAppDataPoint {
 		public required string Name { get; set; }
-		public required double Value { get; set; }
+		public required double? Value { get; set; }
 	}
 }
